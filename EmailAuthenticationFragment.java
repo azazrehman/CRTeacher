7392900edfb214a4b28b0e149f5fd70d7a5c8470
azazrehman <azaz.bscs3173@iiu.ed.pk>
2018-12-22 13:55:14 +0500

    Fragment

---
+++ EmailAuthenticationFragment.java
@@ -0,0 +1,10 @@
+package com.mitsol.project.teachercr;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Master on 22/12/2018 with project TeacherCR.
+ * Contact # +923315665044
+ */
+public class EmailAuthenticationFragment extends Fragment {
+}
---
+++ PhoneAuthenticationFragment.java
@@ -0,0 +1,15 @@
+package com.mitsol.project.teachercr;
+
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Master on 22/12/2018 with project TeacherCR.
+ * Contact # +923315665044
+ */
+public class PhoneAuthenticationFragment extends Fragment {
+
+
+
+
+}
